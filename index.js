3cbb6dbe6266ed9bf6193832398b29f900faef0f
vinieShwan <vinia.chriszon.soldia@gmail.com>
2021-02-11 00:44:24 +0800

    Updated the path to the served address

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(`${__dirname}/build`));
